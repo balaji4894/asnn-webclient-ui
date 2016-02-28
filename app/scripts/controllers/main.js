b9e33432469837788afd90eba2675ace8772b205
balaji4894 <balajiraj1994@gmail.com>
2016-02-28 16:04:24 +0530

    Adding input box and controller to hit the webservice url

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,41 @@
  * Controller of the asnnWebclientUiApp
  */
 angular.module('asnnWebclientUiApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope,$http) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $scope.availableSearchParams = [
+	  { key: "name", name: "Name", placeholder: "Name..." },
+	  { key: "city", name: "City", placeholder: "City...", restrictToSuggestedValues: true, suggestedValues: ['Berlin', 'London', 'Paris'] },
+	  { key: "country", name: "Country", placeholder: "Country..." },
+	  { key: "emailAddress", name: "E-Mail", placeholder: "E-Mail..." },
+	  { key: "job", name: "Job", placeholder: "Job..." }
+	];
+
+	$scope.submitQuery=function(query)
+	{
+		/*$http.get('https://stormy-brushlands-60910.herokuapp.com/query?q="Korattur Chennai"')
+		.success(function(response){
+			$scope.model.result=response;
+		});
+*/
+		var url = 'https://stormy-brushlands-60910.herokuapp.com/query?q="Korattur Chennai"';
+			$http({
+			    method: 'JSONP',
+			    url: url
+			}).
+			success(function(status) {
+			    //your code when success
+			    console.log("success");
+			    $scope.model.result=angular.fromJson(response);
+			    console.log($scope.model);
+			}).
+			error(function(status) {
+			    //your code when fails
+			});
+		};
   });
