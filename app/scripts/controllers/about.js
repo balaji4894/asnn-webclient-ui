0566381ce9380d3ce99a77380c8d012fb34d3a38
balaji4894 <balajiraj1994@gmail.com>
2016-02-28 15:12:39 +0530

    Initial Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name asnnWebclientUiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the asnnWebclientUiApp
+ */
+angular.module('asnnWebclientUiApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
