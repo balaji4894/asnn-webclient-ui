44f6c7f05d46fa3560a7de8d2e2ab38c8a1fd987
balaji4894 <balajiraj1994@gmail.com>
2016-03-13 22:01:40 +0530

    adding modal and progress bars while searching

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -15,6 +15,14 @@ angular.module('asnnWebclientUiApp')
       'Karma'
     ];
 
+    $('.btn').on('click', function() {
+	  var $this = $(this);
+	  $this.button('loading');
+	    setTimeout(function() {
+	       $this.button('reset');
+	   }, 8000);
+	});
+
     $scope.availableSearchParams = [
 	  { key: "name", name: "Name", placeholder: "Name..." },
 	  { key: "city", name: "City", placeholder: "City...", restrictToSuggestedValues: true, suggestedValues: ['Berlin', 'London', 'Paris'] },
@@ -25,9 +33,17 @@ angular.module('asnnWebclientUiApp')
 
 	$scope.submitQuery=function(query)
 	{
+		$scope.model.showresult=false;
+		$scope.model.showProgressModal=true;
 		$http.get('https://stormy-brushlands-60910.herokuapp.com/query?q="'+$scope.model.querystring+'"')
 		.success(function(response){
+			$('.btn').button('reset');
+			$scope.model.showresult=true;
+			$scope.model.showProgressModal=false;
 			$scope.model.result=response;
+		})
+		.error(function(response){
+			$scope.model.showProgressModal=false;
 		});
 
 		/*var url = 'https://stormy-brushlands-60910.herokuapp.com/query?q=Korattur Chennai';
@@ -44,5 +60,16 @@ angular.module('asnnWebclientUiApp')
 			error(function(status) {
 			    //your code when fails
 			});*/
-		};
+	};
+
+	$scope.parseString=function(inputString){
+		var i=0;
+		while(i < inputString.length){
+			if(inputString[i]=='|'){
+				inputString[i]=' ';
+			}
+			i++;
+		}
+		return inputString;
+	}
   });
